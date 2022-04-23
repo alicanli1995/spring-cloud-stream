5aed86b13d55f0aae39eb5ea1afc515c21e74ff4
Ali CANLI <alicanli1995@gmail.com>
2022-04-23 15:08:25 +0300

    init project hellostream

---
+++ hellostreams/src/test/java/com/example/hellostreams/HellostreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellostreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellostreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
