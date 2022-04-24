9884aec0b5994feb56d7fc352993c78017eb5961
Ali CANLI <alicanli1995@gmail.com>
2022-04-24 10:28:36 +0300

    init

---
+++ hellostreams/src/main/java/com/example/hellostreams/bindings/KafkaListenerBinding.java
@@ -0,0 +1,11 @@
+package com.example.hellostreams.bindings;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
+public interface KafkaListenerBinding {
+
+    @Input("input-channel-1")
+    KStream<String ,String > inputStream();
+
+}
---
+++ hellostreams/src/main/java/com/example/hellostreams/services/KafkaListenerService.java
@@ -0,0 +1,22 @@
+package com.example.hellostreams.services;
+
+import com.example.hellostreams.bindings.KafkaListenerBinding;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@EnableBinding(KafkaListenerBinding.class)
+
+public class KafkaListenerService {
+
+    @StreamListener("input-channel-1")
+    public void process(KStream<String ,String > input){
+        input.foreach((k,v) -> log.info(String.format("Key : %s , Value : %s ", k ,v )));
+    }
+
+
+}
